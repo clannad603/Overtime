18aa0cd866dc469ff478033a9d7b8155ea6112ba
huangrui <2817325318@qq.com>
2020-12-13 16:04:55 +0800

    REdrive

--- app/src/main/java/com/example/homework/LoginActivity.java
+++ app/src/main/java/com/example/homework/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.homework;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,6 +10,8 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +19,17 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 
 public class LoginActivity extends BaseActivity {
 private EditText accountEdit;
@@ -67,12 +81,93 @@ private CheckBox rememberPass;
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String account=accountEdit.getText().toString();//get the edit
+                MHandler1 mHandler1=new MHandler1();
+                String account=accountEdit.getText().toString();
                 String password=passwordEdit.getText().toString();
+                HashMap<String, String> map1 = new HashMap<>();
+                map1.put("username", account);
+                map1.put("password",password);
+                sendPostNetRequest("https://www.wanandroid.com/user/login", map1,mHandler1);
+
+            }
+        });
+    }
+    private class MHandler1 extends Handler {
+        @Override public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            String responseData= msg.obj.toString();
+          try {
+              JSONObject jsonObject=new JSONObject(responseData);
+              int errorCode=jsonObject.getInt("errorCode");
+              String errorMsg=jsonObject.getString("errorMsg");
+              JSONObject data=jsonObject.getJSONObject("data");
+              String name=data.getString("username");
+              String pass=data.getString("password");
+              if(errorCode!=0){
+                  Toast.makeText(LoginActivity.this,errorMsg,Toast.LENGTH_SHORT).show();
+              }else{
+                  editor=pref.edit();
+                     if(rememberPass.isChecked()){
+                     editor.putBoolean("remember_password",true);
+                     editor.putString("account",name);
+                     editor.putString("password",pass);
+                     }else{
+                     editor.clear();
+                     }
+                     editor.apply();
+                     Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+              }
+          }catch (Exception e){ e.printStackTrace(); }
+        }
+    }
+    public String StreamToString(InputStream in) {
+        StringBuilder sb = new StringBuilder();
+        String oneLine;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        try {
+            while ((oneLine = reader.readLine()) != null) {
+                sb.append(oneLine).append('\n');
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace(); }
+        }
+
+        return sb.toString();
+    }
+    private void sendPostNetRequest(String mUrl, HashMap<String, String> params,MHandler1 mHandler1) {
+        new Thread( () -> { try {
+            URL url = new URL(mUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(8000);
+            connection.setReadTimeout(8000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            StringBuilder dataToWrite = new StringBuilder();
+            for (String key : params.keySet()) { dataToWrite.append(key).append("=").append(params.get(key)).append("&");
+            }
+            connection.connect();
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(dataToWrite.substring(0, dataToWrite.length() - 1).getBytes());
+            InputStream in = connection.getInputStream();
+            String responseData = StreamToString(in);
+            Message message = new Message();
+            message.obj = responseData;
+            mHandler1.sendMessage(message);
+        } catch (Exception e) { e.printStackTrace(); } } ).start(); }
+        }
 //                SharedPreferences pre = getSharedPreferences("data", MODE_PRIVATE);
 //                SQLiteDatabase db=dbHelper.getWritableDatabase();
 
-               // if(account.equals(pre.getString("name", "10086"))&&password.equals(pre.getString("password", "123456"))){
+// if(account.equals(pre.getString("name", "10086"))&&password.equals(pre.getString("password", "123456"))){
 //             @SuppressLint("Recycle") Cursor cursor=db.query("User",null,null,null,null,null,null);
 //             if(cursor.moveToFirst()) {
 //                 do {
@@ -93,24 +188,20 @@ private CheckBox rememberPass;
 //                         Toast.makeText(LoginActivity.this,"密码或账号无效",Toast.LENGTH_SHORT).show();}
 //                 }while(cursor.moveToNext());
 //             }
-             SharedPreferences pre = getSharedPreferences("data", MODE_PRIVATE);
-             if(account.equals(pre.getString("name", "10086"))&&password.equals(pre.getString("password", "123456"))){
-                    editor=pref.edit();
-                    if(rememberPass.isChecked()){
-                        editor.putBoolean("remember_password",true);
-                        editor.putString("account",account);
-                        editor.putString("password",password);
-                    }else{
-                        editor.clear();
-                    }
-                    editor.apply();
-                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }else{
-                    Toast.makeText(LoginActivity.this,"密码或账号无效",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-}
+//SharedPreferences pre = getSharedPreferences("data", MODE_PRIVATE);
+//             if(account.equals(pre.getString("name", "10086"))&&password.equals(pre.getString("password", "123456"))){
+//                     editor=pref.edit();
+//                     if(rememberPass.isChecked()){
+//                     editor.putBoolean("remember_password",true);
+//                     editor.putString("account",account);
+//                     editor.putString("password",password);
+//                     }else{
+//                     editor.clear();
+//                     }
+//                     editor.apply();
+//                     Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+//        startActivity(intent);
+//        finish();
+//        }else{
+//        Toast.makeText(LoginActivity.this,"密码或账号无效",Toast.LENGTH_SHORT).show();
+//        }
--- app/src/main/java/com/example/homework/Registered.java
+++ app/src/main/java/com/example/homework/Registered.java
@@ -1,5 +1,6 @@
 package com.example.homework;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
@@ -7,6 +8,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +17,18 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 
 public class Registered extends BaseActivity {
     private EditText account,password1,spassword;
@@ -37,24 +52,89 @@ public class Registered extends BaseActivity {
         set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
-                editor.putString("name", account.getText().toString());
+                MHandler mHandler=new MHandler();
+                HashMap<String, String> map = new HashMap<>();
+                map.put("username", account.getText().toString());
+                map.put("password",password1.getText().toString());
+                map.put("repassword",spassword.getText().toString());
+             sendPostNetRequest("https://www.wanandroid.com/user/register", map,mHandler);
+
+              finish();
+            }
+     });
+   }
+    private class MHandler extends Handler {
+        @Override public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            String errorMsg= msg.obj.toString();
+            Toast.makeText(Registered.this,errorMsg,Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void sendPostNetRequest(String mUrl, HashMap<String, String> params,MHandler mHandler) {
+        new Thread( () -> { try {
+            URL url = new URL(mUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(8000);
+            connection.setReadTimeout(8000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            StringBuilder dataToWrite = new StringBuilder();
+            for (String key : params.keySet()) { dataToWrite.append(key).append("=").append(params.get(key)).append("&");
+            }
+            connection.connect();
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(dataToWrite.substring(0, dataToWrite.length() - 1).getBytes());
+            InputStream in = connection.getInputStream();
+            String responseData = StreamToString(in);
+                JSONObject jsonObject=new JSONObject(responseData);
+            int errorCode=jsonObject.getInt("errorCode");
+            String errorMsg=jsonObject.getString("errorMsg");
+          if(errorCode!=0){
+              Message message = new Message();
+              message.obj = errorMsg;
+              mHandler.sendMessage(message);
+//               JSONObject jsonObject1=jsonObject.getJSONObject("data");
+//               String name=jsonObject1.getString("username");
+//              String password=jsonObject1.getString("password");
+          }
+
+        } catch (Exception e) { e.printStackTrace(); } } ).start(); }
+    private String StreamToString(InputStream in) {
+        StringBuilder sb = new StringBuilder();
+        String oneLine;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        try {
+            while ((oneLine = reader.readLine()) != null) {
+                sb.append(oneLine).append('\n');
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace(); }
+        }
+
+        return sb.toString();
+    }
+
+
+}
+//                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+//                editor.putString("name", account.getText().toString());
 //                SQLiteDatabase db=dbHelper.getWritableDatabase();
 //                ContentValues values=new ContentValues();
 //                values.put("user",account.getText().toString());
-                if(password1.getText().toString().equals(spassword.getText().toString())){
+//                if(password1.getText().toString().equals(spassword.getText().toString())){
 //                    values.put("password",password1.getText().toString());
 //                    db.insert("User",null,values);
 //                    values.clear();
-                editor.putString("password", password1.getText().toString());
-                    }
-                else{
-                    Toast.makeText(Registered.this,"两次输入不一致",Toast.LENGTH_SHORT).show();
-                }
-                editor.apply();
-                finish();
-            }
-        });
-    }
-
-}
+//                editor.putString("password", password1.getText().toString());
+//                    }
+//                else{
+//                    Toast.makeText(Registered.this,"两次输入不一致",Toast.LENGTH_SHORT).show();
+//                }
+//                editor.apply();
